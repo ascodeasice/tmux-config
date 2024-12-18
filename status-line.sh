8bd7c4b3478f500aee6cbc31462d783a96b22ddb
ascodeasice <ascodeasice@gmail.com>
2024-12-18 22:06:49 +0800

    fix status line script

--- status-line.sh
+++ status-line.sh
@@ -8,13 +8,19 @@ capacity=$(cat /sys/class/power_supply/BAT1/capacity)
 status=$(cat /sys/class/power_supply/BAT1/status)
 battery_icon=" "
 
-charging_icons=("󰢜 " "󰂆 " "󰂇 " "󰂈 " "󰂉 " "󰂊 " "󰂋 " "󰂅 " "󰂄 ")
-discharging_icons=("󰁺 " "󰁻 " "󰁼 " "󰁽 " "󰁾 " "󰁿 " "󰂀 " "󰁿 " "󰁹 ")
+charging_icons=("󰢜 " "󰂆 " "󰂇 " "󰂈 " "󰢝 " "󰂉 " "󰢞 " "󰂊 " "󰂋 " "󰂄 ")
+discharging_icons=("󰁺 " "󰁻 " "󰁼 " "󰁽 " "󰁾 " "󰁿 " "󰂀 " "󰂁 " "󰂂 " "󰁹 ")
+
+# 100  -> 9, 9x -> 8, x -> 0
 
 # 計算圖示索引（將電池容量除以 10，並向下取整）
-index=$((capacity / 10))
-if [ "$index" -gt 8 ]; then
-	index=8 # 確保索引不超出陣列範圍
+if [ "$capacity" -lt 10 ]; then
+	index=0
+else
+	index=$((capacity / 10 - 1))
+	if [ "$index" -gt 9 ]; then
+		index=9 # 確保索引不超出陣列範圍
+	fi
 fi
 
 # 根據充電狀態選擇對應圖示
@@ -24,17 +30,19 @@ else
 	battery_icon="${discharging_icons[$index]}"
 fi
 
+battery="$battery_icon$capacity%"
+
 internet_icon="󰖪 "
-wired=$(nmcli device status | grep ethernet | grep connected)
-wifi=$(nmcli device status | grep wifi | grep connected)
+
+# NOTE: the space before is needed or else disconnected will also match
+wired=$(nmcli device status | grep ethernet | grep " connected")
+wifi=$(nmcli device status | grep wifi | grep " connected")
 
 if [ -n "$wired" ]; then
 	internet_icon="󰌗 "
 elif [ -n "$wifi" ]; then
 	internet_icon="󰖩 " # seeing wifi strength doesn't provide much value for me, just test its speed
 fi
-
-battery="$battery_icon$capacity%"
 
 # # 取得藍牙連接數量
 # bluetooth_count=$(bluetoothctl devices Connected | wc -l)
